9fe849c1a7379a0a2e7620f1ebe09607933539f4
luigi-zuniga <luis.alberto.zuniga@outlook.com>
2017-11-21 17:29:02 -0300

    uso de funcion convert

--- example6.2.php
+++ example6.2.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
 <html lang="es">
     <head>
-        <title>Example7 | php </title>
+        <title>Example6.2 | php </title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
         <?php
-            /**idem al ejecicio example6, pero esta vez debe de mostrar las fotos en 100x100 y que al pulsar abra la foto entera
-             * Compruebe que solo muestra fotos con extension jpg png bmp o gif
-             * Hacer una funcion que lo compruebe usando las expresiones regulares como aparecen en el manual php
-             */
 
-            echo "<h1>Tabla de Fotos con Enlace</h1>";
+            echo "<h1>Galeria de imagenes con thumbnails</h1>";
 
              function valida_foto($fotos)
              {
                 $rdo = 0;
-                #ereg , comparacion de expresiones regulares.
-                #ESTA FUNCION ESTA OBSOLETA EN PHP5.3 Y ESTA ELIMINADA EN PHP 7.0.0
 
-                if(ereg("[Jj][Pp][Gg]$", $fotos)) rdo=1;
-                if(ereg("[Gg][Ii][Ff]$", $fotos)) rdo=1;
-                if(ereg("[Pp][Nn][Gg]$", $fotos)) rdo=1;
-                if(ereg("[Bb][Mm][Pp]$", $fotos)) rdo=1;
+                #funcion ereg no compatible con php7.0.0
+                if(preg_match("[Jj][Pp][Gg]", $fotos)) rdo=1;
+                if(preg_match("[Gg][Ii][Ff]", $fotos)) rdo=1;
+                if(preg_match("[Pp][Nn][Gg]", $fotos)) rdo=1;
+                if(preg_match("[Bb][Mm][Pp]", $fotos)) rdo=1;
                 
                 return $rdo;
+             }
+
+             function crea_tumbs($foto)
+             {
+                if(!is_dir('img/tumbs'))
+                    mkdir('img/tumbs',0777);
+                if(!is_file('img/tumbs/MINI-$foto'))
+                    system("convert -sample 40x40 /img/$foto /img/tumbs/MINI-$foto");
              }
 
              echo "<table border=1>";
@@ -35,10 +38,12 @@
              {
                  if($foto !="." && $foto !=".." && valida_foto($foto))
                  {
+                     crea_tumbs($foto);
+
                      if($i==1)
                      
                          echo "<tr>";
-                         echo "<td><a href='img/$foto'>";
+                         echo "<td><a href='img/tumbs/MINI-$foto'>";
                          echo "<img src='img/$foto' width=100 height=100></img>";
                          echo "</a></td>";
                          if($i==4){
@@ -48,6 +53,8 @@
             }
             closedir($puntero);
             echo "</table>";
+
+            
         ?>
     </body>
 </html>
